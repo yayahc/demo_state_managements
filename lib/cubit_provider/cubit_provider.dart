58e9588344f2e124b26ab631590c04b940016865
yayahc <yayahc@localhost.localdomain>
2023-08-26 21:44:46 +0000

    state error

--- lib/cubit_provider/cubit_provider.dart
+++ lib/cubit_provider/cubit_provider.dart
@@ -3,4 +3,11 @@ import '../todo.dart';
 
 class CubitProvder extends Cubit<List<Todo>> {
   CubitProvder(super.initialState);
+
+  void createTodo(String content) {
+    final todos = state.add(Todo(state.length + 1, content));
+    emit(todos);
+  }
+
+  void deleteTodo() {}
 }
